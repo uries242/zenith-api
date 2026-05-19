b805b5453bd4292bc1d6229f5486052f7d437841
uries242 <teykoa@gmail.com>
2026-05-19 15:52:16 -0400

    Performed successful server connection test

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+require("dotenv").config();
 require("./db/connection");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
+const mongoose = require("mongoose");
+
+
+//-- MIDDLEWARE --
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+//-- ROUTES --
+app.get("/", (req, res) => {
+  res.send("yeah bey tings solid!")
+});
+
+
+
+//-- PORT --
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+});
