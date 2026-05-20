c5c12296db0ab3f7f0e4d4d82f45a9328570996d
uries242 <teykoa@gmail.com>
2026-05-20 01:24:30 -0400

    Created a DELETE method with error handling to delete a product by its id, tested successfully

--- server.js
+++ server.js
@@ -55,6 +55,21 @@ app.put("/api/products/:id", async (req, res) => {
   }
 });
 
+// Delete a Product
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
 //-- PORT --
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`);
